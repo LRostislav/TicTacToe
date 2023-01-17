08ff4897c0e5ee1a06b116b64abdf790ec094382
Rostislav <rostislav.lysenkov24@gmail.com>
2023-01-17 22:19:08 +0300

    creating a playing area entering and executing a move print of the playing area

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class TicTacToe {
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+
+        // creating a playing area
+        char [][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}
+        };
+
+        printGameBoard(gameBoard);
+
+        // entering a move
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9): ");
+        int pos = scan.nextInt();
+
+        System.out.println(pos);
+
+        // executing a move
+        switch (pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+
+        printGameBoard(gameBoard);
+    }
+
+
+    public static void printGameBoard(char [][] gameBoard){
+        for(char[] row : gameBoard){
+            for(char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
